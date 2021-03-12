58c8d4b0c38811dab80469df0c93e1c513e76555
Michelot Alexis <amichelot@club-internet.fr>
2021-03-12 11:41:29 +0100

    fixed adress server

--- src/useChat.js
+++ src/useChat.js
@@ -4,8 +4,8 @@ import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "chat message";
 const NEW_CONNECTION = "connection";
-// const SOCKET_SERVER_URL = "https://socketchat-backend.herokuapp.com/";
-const SOCKET_SERVER_URL = "http://localhost:3001";
+const SOCKET_SERVER_URL = "https://socketchat-backend.herokuapp.com/";
+// const SOCKET_SERVER_URL = "http://localhost:3001";
 
 const useChat = (name, text) => {
   const [messages, setMessages] = useState([]);
